f2248ad4431a8df7f5cff6678870ab53f98b057a
MassimilianoMancini <massimiliano1.mancini@gmail.com>
2021-11-21 19:50:55 +0100

    sottomatrici

--- Simulazione PI del 2021-01-26/TerzaSimulazionePI.student/src-gen/javamm/ContaSottomatrici.java
+++ Simulazione PI del 2021-01-26/TerzaSimulazionePI.student/src-gen/javamm/ContaSottomatrici.java
@@ -4,11 +4,31 @@ package javamm;
 public class ContaSottomatrici {
   public static int contaSottomatrici(int[][] T, int[][] S) {
     int occ = 0;
-    for (int i = 0; ((i + S.length) < T.length); i++) {
-      for (int j = 0; ((j + S[0].length) < T[0].length); j++) {
-        boolean _tripleEquals = (S == Integer.valueOf(T[i][j]));
-        if (_tripleEquals) {
-          occ++;
+    int k = 0;
+    int l = 0;
+    boolean nextpos = false;
+    for (int i = 0; (((i + S.length) - 1) < T.length); i++) {
+      for (int j = 0; (((j + S[0].length) - 1) < T[0].length); j++) {
+        {
+          nextpos = false;
+          k = 0;
+          while (((k < S.length) && (!nextpos))) {
+            {
+              l = 0;
+              while (((l < S[0].length) && (!nextpos))) {
+                {
+                  boolean _tripleNotEquals = (S[k][l] != T[(k + i)][(l + j)]);
+                  nextpos = _tripleNotEquals;
+                  l++;
+                }
+              }
+              k++;
+            }
+          }
+          boolean _not = (!nextpos);
+          if (_not) {
+            occ++;
+          }
         }
       }
     }
